84e936ddf0eac71ee968687c0b25c11d3da40eef
Søren Ranneries <spinkelben@gmail.com>
2025-11-23 21:33:58 +0100

    Further on the backend food client

--- backed-server/src/main.rs
+++ backed-server/src/main.rs
@@ -1,4 +1,4 @@
-use rocket::build;
+use rocket::{Shutdown, build};
 mod pubq_client;
 
 #[macro_use] extern crate rocket;
@@ -9,10 +9,10 @@ fn index() -> &'static str {
 }
 
 #[get("/test")]
-async fn test() -> Result<String, String> {
+async fn test(mut shutdown: Shutdown) -> Result<String, String> {
     let mut client = pubq_client::PubqClient::new();
     client.connect().await.map_err(|er| format!("Connection failed {:?}", er))?;
-    let first_message = client.receive_message().await.map_err(|er| format!("Receive failed {:?}", er))?;
+    let first_message = client.receive_message(shutdown).await.map_err(|er| format!("Receive failed {:?}", er))?;
     Ok(format!("First message: {}", first_message))
 }
 
--- backed-server/src/pubq_client.rs
+++ backed-server/src/pubq_client.rs
@@ -1,5 +1,6 @@
-use rocket::futures::{StreamExt, TryStreamExt};
+use rocket::{Shutdown, futures::{StreamExt, TryStreamExt}};
 use serde_json::Value;
+use tokio::select;
 
 
 const SOCKET_URL : &str = "wss://s-usc1a-nss-2040.firebaseio.com/.ws?v=5&ns=pq-dev";
@@ -17,13 +18,30 @@ enum MessageData {
     Header(Value),
 }
 
+
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-struct MessageWrapper {
-    #[serde(rename = "t")]
-    r#type: MessageType,
-    #[serde(rename = "d")]
-    data: Value,
+#[serde(tag = "t", content = "d")]
+enum Control {
+    #[serde(rename = "h")]
+    Header { 
+        #[serde(rename = "h")]
+        header: String, 
+        #[serde(rename = "s")]
+        session_id: String, 
+        #[serde(rename = "ts")]
+        timestamp: u64, 
+        #[serde(rename = "v")]
+        version: String },
 }
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[serde(tag = "t", content = "d")]
+enum MessageWrapper {
+    #[serde(rename = "c")]
+    Control(Control),
+    Data(Value),
+}
+
 pub struct PubqClient {
     socket_url: String,
     next_id: u64,
@@ -45,20 +63,55 @@ impl PubqClient {
         Ok(())
     }
 
-    pub async fn receive_message(&mut self) -> Result<String, Box<dyn std::error::Error>> {
+    pub async fn receive_message(&mut self, mut shutdown : Shutdown) -> Result<String, Box<dyn std::error::Error>> {
         let mut messages = vec![];
         if let Some(stream) = &mut self.stream {
-            while let Ok(Some(msg)) = stream.try_next().await {
-                if msg.is_text() {
-                    let text = msg.into_text()?;
-                    let parsed : MessageWrapper = serde_json::from_str(&text)?;
-                    let result = format!("Received message: {:?}\nRaw: {:#?}", parsed, text.to_string());
-                    messages.push(result.clone());
+            loop {
+                select! {
+                    _ = &mut shutdown => {
+                        println!("Shutdown signal received, stopping message reception.");
+                        return Err("Shutdown".into());
+                    },
+                    msg = stream.try_next() =>  {
+                        let msg = match msg? {
+                            Some(m) => m,
+                            None => break,
+                        };
+                        if msg.is_text() {
+                            let text = msg.into_text()?;
+                            println!("Raw message text: {}", text);
+                            let parsed : MessageWrapper = serde_json::from_str(&text)?;
+                            let result = format!("Received message: {:?}", parsed);
+                            println!("{}", result);
+                            messages.push(result);
+                        }
+                    }
                 }
             }
-
+            
             return Ok(messages.join("\n"));
         }
         Err("No message received".into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn can_parse_control_message() {
+        let raw_message = r#"{"t":"c","d":{"t":"h","d":{"ts":1763912344687,"v":"5","h":"s-gke-usc1-nssi1-17.firebaseio.com","s":"OAg9F6yx2JzGq4zZMZqrILfcu6s3AQOX"}}}"#;
+        let parsed: MessageWrapper = serde_json::from_str(raw_message).unwrap();
+        match parsed {
+            MessageWrapper::Control(Control::Header { header, session_id, timestamp, version }) => {
+                assert_eq!(header, "s-gke-usc1-nssi1-17.firebaseio.com");
+                assert_eq!(session_id, "OAg9F6yx2JzGq4zZMZqrILfcu6s3AQOX");
+                assert_eq!(timestamp, 1763912344687);
+                assert_eq!(version, "5");
+            },
+            _ => panic!("Parsed message is not a Control Header"),
+        }
     }
 }
